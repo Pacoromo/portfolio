287777274ba07f01f414fe4c48219a393f298e8d
pacoromo <pacoromo@me.com>
2022-10-22 19:30:35 -0400

    remove scroll when sidebar is open

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 const app = {};
 
-//Elements (Global use)
-app.menuBtn = document.querySelector(".ham-btn");
-app.slideOutMenu = document.querySelector(".side-bar-menu");
-app.contentContainer = document.querySelector(".content-container");
 
 //Sidebar Listeners
 app.sidebarlisteners = () => {
+    app.slideOutMenu = document.querySelector(".side-bar-menu");
+    app.contentContainer = document.querySelector(".content-container");
+    app.body = document.querySelector("body");
+    app.menuBtn = document.querySelector(".ham-btn");
     const mobileNav = document.querySelector(".mobile-nav");
     const mobileMenuLinks = document.querySelectorAll(".side-bar-menu a");
 
-    //On Menu button click
+    //Menu button
     app.menuBtn.addEventListener("click", app.toggleElementsOnActiveSidebar);
 
-    //On any menu item click
-    mobileMenuLinks.forEach((link) => {
+    //Menu items
+        mobileMenuLinks.forEach((link) => {
         link.addEventListener("click", app.toggleElementsOnActiveSidebar);
     });
 
-    //Close mobile menu when user cliks outside of it and it's active
+    //If active, close mobile bar when user cliks outside of it
     document.addEventListener("click", function (e) {
-        if (!mobileNav.contains(e.target) && app.menuBtn.classList.contains("active")) {
+        if (!mobileNav.contains(e.target) && app.menuBtn.classList.contains("active-side-bar")) {
             app.toggleElementsOnActiveSidebar();
         }
     });
@@ -29,9 +29,10 @@ app.sidebarlisteners = () => {
 //Toggle elements on active Sidebar
 
 app.toggleElementsOnActiveSidebar = () => {
-    app.menuBtn.classList.toggle("active");
-    app.slideOutMenu.classList.toggle("active");
-    app.contentContainer.classList.toggle("active");//Blurs content
+    app.menuBtn.classList.toggle("active-side-bar");
+    app.slideOutMenu.classList.toggle("active-side-bar");
+    app.contentContainer.classList.toggle("active-side-bar");//Blurs content
+    app.body.classList.toggle("active-side-bar");//Hide the scroll bar
 };
 
 //Initial
