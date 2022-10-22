77319264b78b51c1d9f12fe94d59a4523697ad1c
pacoromo <pacoromo@me.com>
2022-10-22 13:24:42 -0400

    saved

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
 const app = {};
 
+//Elements (Global use)
+app.menuBtn = document.querySelector(".ham-btn");
+app.slideOutMenu = document.querySelector(".side-bar-menu");
+app.contentContainer = document.querySelector(".content-container");
 
+//Sidebar Listeners
+app.sidebarlisteners = () => {
+    const mobileNav = document.querySelector(".mobile-nav");
+    const mobileMenuLinks = document.querySelectorAll(".side-bar-menu a");
 
+    //On Menu button click
+    app.menuBtn.addEventListener("click", app.toggleElementsOnActiveSidebar);
 
+    //On any menu item click
+    mobileMenuLinks.forEach((link) => {
+        link.addEventListener("click", app.toggleElementsOnActiveSidebar);
+    });
 
-
-
-
-//Button Listener
-app.menuBtnListener = () => {
-    const menuBtn = document.querySelector(".ham-btn");
-    const slideOutMenu = document.querySelector(".side-bar-menu");
-    
-    menuBtn.addEventListener("click", function () {
-        this.classList.toggle("active");
-        slideOutMenu.classList.toggle("active");
+    //Close mobile menu when user cliks outside of it and it's active
+    document.addEventListener("click", function (e) {
+        if (!mobileNav.contains(e.target) && app.menuBtn.classList.contains("active")) {
+            app.toggleElementsOnActiveSidebar();
+        }
     });
 };
 
+//Toggle elements on active Sidebar
+
+app.toggleElementsOnActiveSidebar = () => {
+    app.menuBtn.classList.toggle("active");
+    app.slideOutMenu.classList.toggle("active");
+    app.contentContainer.classList.toggle("active");//Blurs content
+};
+
+//Initial
+
 app.init = () => {
-    app.menuBtnListener();
+    app.sidebarlisteners();
 };
 
 app.init();
