73945bdd2b66efec1c0359e6df7f853217a786e3
pacoromo <pacoromo@me.com>
2022-10-23 12:29:01 -0400

    corrected headings

--- js/script.js
+++ js/script.js
@@ -1,38 +1,26 @@
 const app = {};
 
-app.options = {
-    root: null,
-    rootMargin: "0px 0px",
-    threshold: 0.5
-}
-
-app.observer = new IntersectionObserver(beTouching, options);
-app.observer.observe();
-
-
 //Listeners
 app.listeners = () => {
     app.body = document.querySelector("body");
     app.contentContainer = document.querySelector(".content-container");
     app.menuBtn = document.querySelector(".ham-btn");
     app.slideOutMenu = document.querySelector(".side-bar-menu");
-
-    const mobileNav = document.querySelector(".mobile-nav");
-    const mobileMenuLinks = document.querySelectorAll(".side-bar-menu a");
-    const skills = document.querySelectorAll(".social-side-bar li");
+    app.mobileNav = document.querySelector(".mobile-nav");
+    app.mobileMenuLinks = document.querySelectorAll(".side-bar-menu a");
 
     //Menu button
     app.menuBtn.addEventListener("click", app.toggleElementsOnActiveSidebar);
 
     //Menu items
-    mobileMenuLinks.forEach((link) => {
+    app.mobileMenuLinks.forEach((link) => {
         link.addEventListener("click", app.toggleElementsOnActiveSidebar);
     });
 
     //If active, close mobile bar when user cliks outside of it
     document.addEventListener("click", function (e) {
         if (
-            !mobileNav.contains(e.target) &&
+            !app.mobileNav.contains(e.target) &&
             app.menuBtn.classList.contains("active-side-bar")
         ) {
             app.toggleElementsOnActiveSidebar();
@@ -41,15 +29,12 @@ app.listeners = () => {
 
     //Remove the active state if viewport is beyond medium screen size
     window.addEventListener("resize", () => {
-        if (this.innerWidth >= 768 && app.menuBtn.classList.contains("active-side-bar")) {
+        if (
+            this.innerWidth >= 768 &&
+            app.menuBtn.classList.contains("active-side-bar")
+        ) {
             app.toggleElementsOnActiveSidebar();
         }
-    });
-
-    //Observe elements to animate
-    skills.forEach(element => {
-        observer.observe(element);
-        console.log(element);
     });
 };
 
@@ -62,10 +47,41 @@ app.toggleElementsOnActiveSidebar = () => {
     app.slideOutMenu.classList.toggle("active-side-bar");
 };
 
+//Observers
+
+app.startObservers = () => {
+
+    app.options = {
+        root: null,
+        rootMargin: "0px 0px",
+        threshold: 0,
+    };
+
+    app.beTouching = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active-fade-in");
+                app.observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    app.observer = new IntersectionObserver(app.beTouching, app.options);
+    app.fadeInElements = document.querySelectorAll(".fade-in");
+
+    //Observe elements to animate
+    app.fadeInElements.forEach((element) => {
+        element.style.opacity = 0;
+        app.observer.observe(element);
+    });
+
+};
+
 //Initial
 
 app.init = () => {
     app.listeners();
+    app.startObservers();
 };
 
 app.init();
