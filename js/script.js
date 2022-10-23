ba09c7fe7a2766a21f6682b02970996360b76926
pacoromo <pacoromo@me.com>
2022-10-22 21:39:36 -0400

    svgs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const app = {};
 
-
-//Sidebar Listeners
-app.sidebarlisteners = () => {
-    app.slideOutMenu = document.querySelector(".side-bar-menu");
-    app.contentContainer = document.querySelector(".content-container");
+//Listeners
+app.listeners = () => {
     app.body = document.querySelector("body");
+    app.contentContainer = document.querySelector(".content-container");
     app.menuBtn = document.querySelector(".ham-btn");
+    app.slideOutMenu = document.querySelector(".side-bar-menu");
+
     const mobileNav = document.querySelector(".mobile-nav");
     const mobileMenuLinks = document.querySelectorAll(".side-bar-menu a");
 
@@ -14,13 +14,23 @@ app.sidebarlisteners = () => {
     app.menuBtn.addEventListener("click", app.toggleElementsOnActiveSidebar);
 
     //Menu items
-        mobileMenuLinks.forEach((link) => {
+    mobileMenuLinks.forEach((link) => {
         link.addEventListener("click", app.toggleElementsOnActiveSidebar);
     });
 
     //If active, close mobile bar when user cliks outside of it
     document.addEventListener("click", function (e) {
-        if (!mobileNav.contains(e.target) && app.menuBtn.classList.contains("active-side-bar")) {
+        if (
+            !mobileNav.contains(e.target) &&
+            app.menuBtn.classList.contains("active-side-bar")
+        ) {
+            app.toggleElementsOnActiveSidebar();
+        }
+    });
+
+    //Remove the active state if viewport is beyond medium screen size
+    window.addEventListener("resize", () => {
+        if (this.innerWidth >= 768 && app.menuBtn.classList.contains("active-side-bar")) {
             app.toggleElementsOnActiveSidebar();
         }
     });
@@ -29,16 +39,16 @@ app.sidebarlisteners = () => {
 //Toggle elements on active Sidebar
 
 app.toggleElementsOnActiveSidebar = () => {
+    app.body.classList.toggle("active-side-bar"); //Hide the scroll bar
+    app.contentContainer.classList.toggle("active-side-bar"); //Blurs content
     app.menuBtn.classList.toggle("active-side-bar");
     app.slideOutMenu.classList.toggle("active-side-bar");
-    app.contentContainer.classList.toggle("active-side-bar");//Blurs content
-    app.body.classList.toggle("active-side-bar");//Hide the scroll bar
 };
 
 //Initial
 
 app.init = () => {
-    app.sidebarlisteners();
+    app.listeners();
 };
 
 app.init();
